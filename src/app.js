e7bc5ec1b3eb7108e86c12f720b67d63f1446c17
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-19 22:44:16 +0400

    Feat(*): Added screens functional

--- src/app.js
+++ src/app.js
@@ -1,7 +1,37 @@
 const start = document.getElementById('start')
 const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('.time-list')
+let time = 0
 
 start.addEventListener('click', (evt) => {
   evt.preventDefault()
   screens[0].classList.add('up')
 })
+
+timeList.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('time-list__item')) {
+    time = parseInt(evt.target.dataset.time)
+    screens[1].classList.add('up')
+    startGame()
+  }
+})
+
+const timeEl = document.getElementById('time')
+
+function startGame() {
+  setTime(time)
+  setInterval(decreaseTime, 1000)
+}
+
+function decreaseTime() {
+  if (time === 0) {
+  } else {
+    currentTime = --time
+    if (currentTime < 10) currentTime = `0${currentTime}`
+    setTime(currentTime)
+  }
+}
+
+function setTime(value) {
+  timeEl.innerHTML = `00:${value}`
+}
